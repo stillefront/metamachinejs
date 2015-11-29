eb4d241e6708196126a7828a07f30abad74cb2d5
alexej <alexej@stillefront.de>
2015-11-29 17:28:19 +0100

    added simple debug console and fiddled around with xml parsing through XMLHttpRequest

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
-/* the main file of metamachine.js
-   */
+// the main file of metamachine.js
+
+xmlhttp = new XMLHttpRequest();
+	xmlhttp.open("GET","xml/metadata.xml",false);
+        xmlhttp.send();
+        xmlDoc = xmlhttp.responseXML;
+
+	document.getElementById("clipname").innerHTML=xmlDoc.getElementsByTagName("name")[0].childNodes[0].nodeValue;
+	document.getElementById("begin").innerHTML=xmlDoc.getElementsByTagName("pos_vorne")[0].childNodes[0].nodeValue;
+	document.getElementById("middle").innerHTML=xmlDoc.getElementsByTagName("pos_mitte")[0].childNodes[0].nodeValue;
+	document.getElementById("end").innerHTML=xmlDoc.getElementsByTagName("pos_hinten")[0].childNodes[0].nodeValue;
+
