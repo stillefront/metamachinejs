d6ba78f7cf9027ac2f92ff3a5c0e25e59cfb3608
alexejb <stillefront@users.noreply.github.com>
2016-01-04 16:15:49 +0100

    figured out ajax in jQuery and implemented simple import for XML-data from files

--- querymain.js
+++ querymain.js
@@ -2,24 +2,54 @@
 
 $(document).ready(function(){
 
-
-var title, attr1, attr2, attr3, attr4, attr5, attr6, posBegin, posMiddle, posEnd;
-
 $.ajax({
-	url: '/xml/metadata.xml',
-	type: 'GET',
-	dataType: 'xml',
-		success: function(returnedXMLResponse){
-			$('clip', returnedXMLResponse).each(function(){
-				title = $('name', this).text();
-				attr1 = $('abenteuer', this).text();
-				posBegin = $('pos_vorne', this).text(); 
-			})
+	type: "GET",
+	url: "metadata.xml",
+	dataType: "xml",
+		success:function(xml){
+		$(xml).find('clip').each(function(){
+		
+		var pos_begin = $(this).find('pos_vorne').text(),
+	    	    pos_middle = $(this).find('pos_mitte').text(),
+	    	    pos_end = $(this).find('pos_hinten').text(),
+	    	    abenteuer = $(this).find('abenteuer').text(),
+	    	    name = $(this).find('name').text();
+		
+	$("#XMLread").append('<li>' +pos_begin+ '-' +pos_middle+ '-' +pos_end+ '-' +abenteuer+ '-' +name+ '</li>');
+		
+		})
+		
 		}
+
+
 
 });
 
 
+
+
+
+
+/*
+var xmlDoc = $.parseXML("/xml/metadata.xml");
+var $xml = $(xmlDoc);
+
+var $clip = $xml.find("clip");
+
+$clip.each(function(){
+	var pos_begin = $(this).find('pos_vorne').text(),
+	    pos_middle = $(this).find('pos_mitte').text(),
+	    pos_end = $(this).find('pos_hinten').text(),
+	    abenteuer = $(this).find('abenteuer').text(),
+	    name = $(this).find('name').text();
+
+	$("#XMLread").append('<li>' +pos_begin+ '-' +pos_middle+ '-' +pos_end+ '-' +abenteuer+ '-' +name+ '</li>');
+
+
+
+		});
+
+*/
 
 })
 
