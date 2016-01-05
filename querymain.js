6b1b47ec854ce661399c0c8327205225cbe438fe
alexejb <stillefront@users.noreply.github.com>
2016-01-05 17:51:08 +0100

    sorting videos into appropriate position-arrays

--- querymain.js
+++ querymain.js
@@ -2,18 +2,61 @@
 
 $(document).ready(function(){
 
+var pos_begin,
+    pos_middle,
+    pos_end,
+    abenteuer,
+    geborgenheit,
+    koerpergenuss,
+    kuligenuss,
+    chance,
+    geselligkeit,
+    name;	    
+
+var begin = [];
+var middle = [];
+var end = [];
+
+var random_Begin = [];
+var random_Middle = [];
+var random_End = [];
+
+var playlist = [];
+
+
+
+ 
 $.ajax({
 	type: "GET",
 	url: "metadata.xml",
 	dataType: "xml",
+
 		success:function(xml){
 		$(xml).find('clip').each(function(){
 		
-		var pos_begin = $(this).find('pos_vorne').text(),
-	    	    pos_middle = $(this).find('pos_mitte').text(),
-	    	    pos_end = $(this).find('pos_hinten').text(),
-	    	    abenteuer = $(this).find('abenteuer').text(),
+		    pos_begin = $(this).find('pos_vorne').text();
+	    	    pos_middle = $(this).find('pos_mitte').text();
+	    	    pos_end = $(this).find('pos_hinten').text();
+	    	    abenteuer = $(this).find('abenteuer').text();
+		    geborgenheit =  $(this).find('geborgenheit').text();
+		    koerpergenuss = $(this).find('koer_genuss').text();
+		    kuligenuss =  $(this).find('kulin_genuss').text();
+		    chance =  $(this).find('chance').text();
+		    geselligkeit =  $(this).find('geselligkeit').text();
 	    	    name = $(this).find('name').text();
+
+		    if (pos_begin === "true") {
+		    begin.push(name);
+		    } 
+		    if (pos_middle === "true") {
+		    middle.push(name);
+		    } 
+		    if (pos_end === "true") {
+		    end.push(name);
+		    }
+		    console.log("begin:", begin);
+		    console.log("middle", middle);
+		    console.log("end", end);
 		
 	$("#XMLread").append('<li>' +pos_begin+ '-' +pos_middle+ '-' +pos_end+ '-' +abenteuer+ '-' +name+ '</li>');
 		
@@ -24,32 +67,6 @@ $.ajax({
 
 
 });
-
-
-
-
-
-
-/*
-var xmlDoc = $.parseXML("/xml/metadata.xml");
-var $xml = $(xmlDoc);
-
-var $clip = $xml.find("clip");
-
-$clip.each(function(){
-	var pos_begin = $(this).find('pos_vorne').text(),
-	    pos_middle = $(this).find('pos_mitte').text(),
-	    pos_end = $(this).find('pos_hinten').text(),
-	    abenteuer = $(this).find('abenteuer').text(),
-	    name = $(this).find('name').text();
-
-	$("#XMLread").append('<li>' +pos_begin+ '-' +pos_middle+ '-' +pos_end+ '-' +abenteuer+ '-' +name+ '</li>');
-
-
-
-		});
-
-*/
 
 })
 
