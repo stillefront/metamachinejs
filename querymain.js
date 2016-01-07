4843c1c1478f1134d90e3218d3d7afd253f01767
alexejb <stillefront@users.noreply.github.com>
2016-01-07 11:22:25 +0100

    for loops

--- querymain.js
+++ querymain.js
@@ -5,12 +5,6 @@ $(document).ready(function(){
 var pos_begin,
     pos_middle,
     pos_end,
-    abenteuer,
-    geborgenheit,
-    koerpergenuss,
-    kuligenuss,
-    chance,
-    geselligkeit,
     name;	    
 
 var begin = [];
@@ -28,12 +22,17 @@ var playlist = [];
 $('#generatePlaylist').click(function(event){
 
 // fetch parameters from HTML5 range sliders attr1 to attr6
-var attr1 = $('#attr1').val();
-var attr2 = $('#attr2').val();
-var attr3 = $('#attr3').val();
-var attr4 = $('#attr4').val();
-var attr5 = $('#attr5').val();
-var attr6 = $('#attr5').val();
+var attr = [];
+
+attr[0] = $('#attr1').val(); 
+attr[1] = $('#attr2').val();
+attr[2] = $('#attr3').val();
+attr[3] = $('#attr4').val();
+attr[4] = $('#attr5').val();
+attr[5] = $('#attr6').val();
+
+
+var attrXML = []; // for comparison
 
 // reset Arrays
 begin.length = 0;
@@ -54,76 +53,40 @@ $.ajax({
 		    pos_begin = $(this).find('pos_vorne').text();
 	    	    pos_middle = $(this).find('pos_mitte').text();
 	    	    pos_end = $(this).find('pos_hinten').text();
-	    	    abenteuer = $(this).find('abenteuer').text();
-		    geborgenheit =  $(this).find('geborgenheit').text();
-		    koerpergenuss = $(this).find('koer_genuss').text();
-		    kuligenuss =  $(this).find('kulin_genuss').text();
-		    chance =  $(this).find('chance').text();
-		    geselligkeit =  $(this).find('geselligkeit').text();
+	    	    attrXML[0] = $(this).find('abenteuer').text();
+		    attrXML[1] =  $(this).find('geborgenheit').text();
+		    attrXML[2] = $(this).find('koer_genuss').text();
+		    attrXML[3] =  $(this).find('kulin_genuss').text();
+		    attrXML[4] =  $(this).find('chance').text();
+		    attrXML[5] =  $(this).find('geselligkeit').text();
 	    	    name = $(this).find('name').text();
 
-		    // get suitable videos for the beginning; NOTE: IMPLEMENT THOSE AS LOOPS IN THE FUTURE!
-
-		    if (abenteuer == attr1 && pos_begin == "true") {
-		    begin.push(name);
-		    } 
-		    if (geborgenheit == attr2 && pos_begin == "true") {
-		    begin.push(name);
-		    } 
-		    if (koerpergenuss == attr3 && pos_begin == "true") {
-		    begin.push(name);
-		    } 
-		    if (kuligenuss == attr4 && pos_begin == "true") {
-		    begin.push(name);
-		    } 
-		    if (chance == attr5 && pos_begin == "true") {
-		    begin.push(name);
-		    } 
-		    if (geselligkeit == attr6 && pos_begin == "true") {
-		    begin.push(name);
-		    } 
+		    // get suitable videos for the beginning
+		    for(i = 0; i < attr.length; i++){
+		    
+			    if (attrXML[i] == attr[i] && pos_begin == "true") {
+			    begin.push(name);
+			    }
+		    
+		    } // end for
 
 		    // get suitable videos for the middlepart
+		    for(i = 0; i < attr.length; i++){
+		    
+			    if (attrXML[i] == attr[i] && pos_middle == "true") {
+			    middle.push(name);
+			    }
+		    
+		    } // end for
 
-		    if (abenteuer == attr1 && pos_middle == "true") {
-		    middle.push(name);
-		    }
-		    if (geborgenheit == attr2 && pos_middle == "true") {
-		    middle.push(name);
-		    }
-		    if (koerpergenuss == attr3 && pos_middle == "true") {
-		    middle.push(name);
-		    }
-		    if (kuligenuss == attr4 && pos_middle == "true") {
-		    middle.push(name);
-		    }
-		    if (chance == attr5 && pos_middle == "true") {
-		    middle.push(name);
-		    }
-		    if (geselligkeit == attr6 && pos_middle == "true") {
-		    middle.push(name);
-		    }
-
-		    // get suitable videos for the endpart
-
-		    if (abenteuer == attr1 && pos_end == "true") {
-		    end.push(name);
-		    }
-		    if (geborgenheit == attr2 && pos_end == "true") {
-		    end.push(name);
-		    }
-		    if (koerpergenuss == attr3 && pos_end == "true") {
-		    end.push(name);
-		    }
-		    if (kuligenuss == attr4 && pos_end == "true") {
-		    end.push(name);
-		    }
-		    if (chance == attr5 && pos_end == "true") {
-		    end.push(name);
-		    }
-		    if (geselligkeit == attr6 && pos_end == "true") {
-		    end.push(name);
-		    }
+		    // get suitable videos for the end
+		    for(i = 0; i < attr.length; i++){
+		    
+			    if (attrXML[i] == attr[i] && pos_end == "true") {
+			    end.push(name);
+			    }
+		    
+		    } // end for
 
 
 		    console.log("begin:", begin);
@@ -188,7 +151,7 @@ $.ajax({
 
 
 
-		    console.log("Ticket:", attr1, attr2, attr3, attr4, attr5, attr6);
+		    console.log("Ticket:", attr[0], attr[1], attr[2], attr[3], attr[4], attr[5]);
 		
 		
 		})
